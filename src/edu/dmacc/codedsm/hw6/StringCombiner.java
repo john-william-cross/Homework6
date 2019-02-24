f5833fe57507bb41ff5168319ac6aca7fdf428e4
John Cross <jwcross1@dmacc.edu>
2019-02-24 12:55:37 -0600

    initial commit

---
+++ src/edu/dmacc/codedsm/hw6/StringCombiner.java
@@ -0,0 +1,35 @@
+package edu.dmacc.codedsm.hw6;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class StringCombiner {
+
+    public static void main(String[] args) {
+
+        String stringOne = "Today";
+        String stringTwo = "is";
+
+        String twoStrings = twoStrings(stringOne, stringTwo);
+
+        String result = combinedMethods(twoStrings, outputDate());
+
+        System.out.println(result);
+
+    }
+
+    public static String twoStrings(String stringOne, String stringTwo) {
+        return stringOne + " " + stringTwo;
+    }
+
+    public static String outputDate() {
+        SimpleDateFormat format = new SimpleDateFormat("dd MMMM, yyyy.");
+        return format.format(Date.from(Instant.now()));
+    }
+
+    public static String combinedMethods(String twoStrings, String outputDate) {
+        return twoStrings + " " + outputDate;
+    }
+
+}
