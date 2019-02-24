583f3faad42a5087926f2a89ea0c9e94bb5339db
John Cross <jwcross1@dmacc.edu>
2019-02-24 16:48:19 -0600

    initial commit

--- src/edu/dmacc/codedsm/hw6/StringCombiner.java
+++ src/edu/dmacc/codedsm/hw6/StringCombiner.java
@@ -7,27 +7,16 @@ import java.util.Date;
 public class StringCombiner {
 
     public static void main(String[] args) {
-        String stringOne = "Today";
-        String stringTwo = "is";
 
-        String twoStrings = twoStrings(stringOne, stringTwo);
-
-        String result = combinedMethods(twoStrings, outputDate());
-
-        System.out.println(result);
+        System.out.println(allStrings(allStrings("Today", "is"), outputDate()));
     }
 
-    public static String twoStrings(String stringOne, String stringTwo) {
-        return stringOne + " " + stringTwo;
+    public static String allStrings(String allStrings, String outputDate) {
+        return allStrings + " " + outputDate;
     }
 
     public static String outputDate() {
         SimpleDateFormat format = new SimpleDateFormat("dd MMMM, yyyy.");
         return format.format(Date.from(Instant.now()));
     }
-
-    public static String combinedMethods(String twoStrings, String outputDate) {
-        return twoStrings + " " + outputDate;
-    }
-
 }
